b53d714bfb079ef8b635c005758d0f4e26b62a87
Orest <notmemeapp@gmail.com>
2024-08-02 15:17:17 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -28,6 +28,8 @@ app.use("/api/messages", messageRoutes);
 const server = app.listen(process.env.PORT, () =>
   console.log(`Server started on ${process.env.PORT}`)
 );
+
+
 const io = socket(server, {
   cors: {
     origin: "http://localhost:3000",
